332a292b666859645dd1ffd3a5ddb2fb59d43edb
luningcowboy <luningcowboy@gmail.com>
2019-06-11 13:35:47 +0800

    条件语句

---
+++ code/base/conditional.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+flag = False
+name = 'luren'
+if name == 'python':
+    flat = True
+    print 'welcome boss'
+else:
+    print name
+
+num = 5
+if num == 3:
+    print 'boss'
+elif num == 2:
+    print 'user'
+elif num == 1:
+    print 'worker'
+elif num < 0:
+    print 'error'
+else:
+    print 'roadam'
+
+num = 9
+if num >= 0 and num <= 10:
+    print 'hello'
+else:
+    print 'undefine'
+
+num = 8
+if (num >= 0 and num <= 5) or (num >= 10 and num <= 15):
+    print 'hello'
+else:
+    print 'undefine'
+
+var = 100
+if (var == 100): print 'var = 100'
+print 'Good Bye'
