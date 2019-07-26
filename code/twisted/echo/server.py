af1316759f5199cfba8ff76cd17c8a8d8405fffb
luningcowboy <luningcowboy@gmail.com>
2019-07-26 17:40:23 +0800

    add twisted demo

---
+++ code/twisted/echo/server.py
@@ -0,0 +1,14 @@
+from twisted.internet import reactor, protocol
+
+class Echo(protocol.Protocol):
+    def dataReceived(self, data):
+        self.transport.write(data)
+
+def main():
+    factory = protocol.ServerFactory()
+    factory.protocol = Echo
+    reactor.listenTCP(8000, factory)
+    reactor.run()
+
+if __name__ == '__main__':
+    main()
