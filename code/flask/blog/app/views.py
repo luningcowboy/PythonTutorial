e66f4bb5e592af92e8baa34648bdd96153ac18f9
luningcowboy <luningcowboy@gmail.com>
2019-10-14 13:12:23 +0800

    add flash

--- code/flask/blog/app/views.py
+++ code/flask/blog/app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
@@ -20,4 +20,7 @@ def index():
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requreted for OpenID=' + form.openid.data + ', remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html',title="Sign In",form=form)
