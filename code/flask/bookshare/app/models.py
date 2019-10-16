c55a27de180a73b6cc0816d9cc86341f545278cb
luningcowboy <luningcowboy@gmail.com>
2019-10-16 10:09:31 +0800

    new code

--- code/flask/bookshare/app/models.py
+++ code/flask/bookshare/app/models.py
@@ -3,9 +3,13 @@ from app import db
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    description = db.Column(db.String)
+    desc = db.Column(db.String)
     tag = db.Column(db.Integer)
     url = db.Column(db.String)
     author = db.Column(db.String)
+    pic_url = db.Column(db.String)
     def __repr__(self):
         return '<Book %r>'%(self.name)
+
+def getBooksByTag(tag):
+    pass
--- code/flask/bookshare/app/views.py
+++ code/flask/bookshare/app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, url_for
 from app import app
 
 books = [
@@ -7,13 +7,17 @@ books = [
         {'name':'呐喊3','author':'鲁迅3','url':'http://www.baidu.com'},
         {'name':'呐喊4','author':'鲁迅4','url':'http://www.baidu.com'}
         ]
+types = [
+        {'name':'计算机','url':'type','tag':'1'},
+        {'name':'小说','url':'type','tag':'2'}]
 
 @app.route("/")
 @app.route("/index/")
 def index():
-    return render_template('index.html')
+    for type in types:
+        type['url'] = url_for(type['url'], type=type['tag'])
+    return render_template('index.html',types=types)
 
 @app.route("/type/<type>")
 def type(type):
-    return render_template('type.html', type=type,books=books)
-    #return "type:%s" % type
+    return render_template('type.html', type=type,books=books,types=types)
