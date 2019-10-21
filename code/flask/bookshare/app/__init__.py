17a651567db9be73d6e2743df3f048af9cee615d
luningcowboy <luningcowboy@gmail.com>
2019-10-21 22:18:09 +0800

    add db interface

--- code/flask/bookshare/app/__init__.py
+++ code/flask/bookshare/app/__init__.py
@@ -13,3 +13,31 @@ def initDB(drop):
         db.drop_all()
     db.create_all()
     click.echo('Initialized database.')
+
+@app.cli.command()
+@click.option('--config', default='./book.json', help='Add books from json')
+def addBook(config):
+    click.echo('add Book from config file:%s'% config)
+    """
+    "name": "book3",
+    "author": "author3",
+    "pic": "wwww.baidu.com",
+    "desc": "This is description",
+    "contents": "描\n述\n文\n本",
+    "type": "type",
+    "download": "www.baidu.com,www.baidu.com"
+    """
+    import json as js
+    bookConfig = open(config).read()
+    bookConfig = js.loads(bookConfig)
+    click.echo(bookConfig['books'])
+    for book in bookConfig['books']:
+        click.echo(book['name'])
+        bookData = models.Book(name=book['name'],
+                author=book['author'],
+                booktype=book['type'],
+                pic=book['pic'],
+                contents=book['contents'],
+                download=book['download'])
+        db.session.add(bookData)
+        db.session.commit()
--- code/flask/bookshare/app/models.py
+++ code/flask/bookshare/app/models.py
@@ -1,32 +1,12 @@
 from app import db
-"""
-'name':'呐喊1'
-'author':'鲁迅1'
-'id':0
-'desc':'这是一本关于呐喊的书'
-'contents':['xxxxx','xxxx','xxxx']
-'download':['https://www.baidu.com','https://www.baidu.com']
-'pic_url':'http://haodoo.net/covers/17Z7.jpg'
-'state':'是否可用'
-"""
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    tag = db.Column(db.Integer)
+    booktype = db.Column(db.String)
     download = db.Column(db.String)
-    author = db.Column(db.String)
-    contents = db.Column(db.String)
+    author = db.Column(db.String) 
+    contents = db.Column(db.Text)
     pic = db.Column(db.String)
     def __repr__(self):
         return '<Book %r>'%(self.name)
-class BookType(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    tag = db.Column(db.Integer)
-    name = db.Column(db.String)
-    subtag = db.Column(db.Integer)
-    def __repr__(self):
-        return '<BookType %r>'%(self.name)
-
-def getBooksByTag(tag):
-    pass
 
