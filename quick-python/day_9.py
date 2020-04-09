461e286fdd187e54c8cb6c72206a30cfd4e2f604
luningcowboy <luningcowboy@gmail.com>
2020-04-09 21:18:24 +0800

    modify note

--- quick-python/day_9.py
+++ quick-python/day_9.py
@@ -8,54 +8,54 @@
 """
 # 删除列表元素
 l1 = [1, 2, 3, 4]
-print(l1)
+print(l1) # [1,2,3,4]
 del l1[0]
-print(l1)
+print(l1) # [2,3,4]
 # 获取列表长度
-print(len(l1))
+print(len(l1)) # 3
 # 列表组合
 l2 = [11, 12, 13]
 l3 = l1 + l2
-print(l3)
+print(l3) #[2,3,4, 11, 12, 13]
 # 列表重复
-print(l2 * 3)
+print(l2 * 3) # [11, 12, 13, 11, 12, 13, 11, 12, 13]
 # 判断元素是否存在于列表中
-print(11 in l2)
-print(11 not in l2)
+print(11 in l2) # True
+print(11 not in l2) # False
 # 列表迭代
 for x in l2:
-    print(x, end=" ")
+    print(x, end=" ") # 11 12 13 11 12 13 11 12 13
 # 列表截取和分割
-print(l2[1])
-print(l2[-2])
-print(l2[1:])
+print(l2[1]) # 12
+print(l2[-2]) # 12
+print(l2[1:]) # 12 13 11 12 13 11 12 13
 # 嵌套列表
 l4 = [[1,2],3,4,[5,6]]
-print(l4)
-print(l4[0])
+print(l4) #[[1,2],3,4,[5,6]]
+print(l4[0]) # [1, 2]
 # 返回列表最大元素值
-print(max(l2))
+print(max(l2)) # 13
 # 获取列表最小值
-print(min(l2))
+print(min(l2)) # 11
 # 元组转换为列表
-print(list((1,2,3)))
+print(list((1,2,3))) # [1, 2, 3]
 # 列表操作
 l5 = []
 # 添加元素
 l5.append(1)
 l5.append(1)
-print(l5)
+print(l5) # [1, 1]
 # 统计某个元素的数量
-print(l5.count(1))
+print(l5.count(1)) # 2
 # 追加元素
 l5.extend(range(5))
-print(l5)
+print(l5) # [1, 1, 0, 1, 2, 3, 4]
 l5.extend([3,2,1])
-print(l5)
+print(l5) # [1, 1, 0, 1, 2, 3, 4, 3, 2, 1]
 # 返回列表中指定元素的第一个匹配项的索引值
-print(l5.index(3))
+print(l5.index(3)) # 5
 # 插入
-l5.insert(1, 'abc')
+l5.insert(1, 'abc') #
 print(l5)
 # 移除列表的一个元素并返回其值,默认最后一个元素
 print(l5.pop())
@@ -83,7 +83,7 @@ random = [(2,2),(3,4),(4,5),(5,1)]
 random.sort(key=takeSecond)
 print(random)
 
-# 情况列表
+# 清空列表 
 l8 = [1,2,3]
 print(l8)
 l8.clear()
@@ -93,6 +93,6 @@ print(l8)
 l9 = [1,2,3,4]
 l10 = l9.copy()
 l11 = [1,2,3,4]
-print(l10)
-print(l10 == l9)
-print(l11 == l9)
+print(l10) # [1,2,3,4]
+print(l10 == l9) # True
+print(l11 == l9) # True
